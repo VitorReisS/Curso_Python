09d8ae9522e66d6e9145784f5f727147ed2ce646
VitorReisS <vitor20101983@gmail.com>
2024-07-31 10:58:05 -0300

    Solução do Exercício - sistema de perguntas e respostas com Python

--- Programacao_Procedural_Funcional/aula77.py
+++ Programacao_Procedural_Funcional/aula77.py
@@ -1,5 +1,6 @@
 # Exercício - sistema de perguntas e respostas
 
+
 perguntas = [
     {
         'Pergunta': 'Quanto é 2+2?',
@@ -17,3 +18,40 @@ perguntas = [
         'Resposta': '5',
     },
 ]
+
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta:', pergunta['Pergunta'])
+    print()
+
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+    print()
+
+    escolha = input('Escolha uma opção: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isnumeric():
+        escolha_int = int(escolha)
+
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou ✅')
+    else:
+        print('Errou ❌')
+
+    print()
+
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
