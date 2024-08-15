515429dd5057b7a1992ed51a376fa278f841e4eb
VitorReisS <vitor20101983@gmail.com>
2024-08-15 09:05:11 -0300

    O ponto de vista do __main__ pode te confundir em módulos e pacotes Python

--- Programacao_Procedural_Funcional/aula99_package/modulo.py
+++ Programacao_Procedural_Funcional/aula99_package/modulo.py
@@ -3,6 +3,7 @@
 #     'soma_do_modulo',
 #     'nova_variavel',
 # ]
+from aula99_package.modulo_b import fala_oi
 
 variavel = 'Alguma coisa'
 
@@ -10,3 +11,4 @@ def soma_do_modulo(x, y):
     return x + y
 
 nova_variavel = 'OK'
+# fala_oi()
