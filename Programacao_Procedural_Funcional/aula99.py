1302e7a5de479d340d2a21aff2869171bc949aca
VitorReisS <vitor20101983@gmail.com>
2024-08-15 09:50:36 -0300

    __init__.py é um arquivo de inicialização dos packages em Python

--- Programacao_Procedural_Funcional/aula99.py
+++ Programacao_Procedural_Funcional/aula99.py
@@ -12,7 +12,7 @@
 # print(modulo.soma_do_modulo(1, 2))
 # print(variavel)
 # print(nova_variavel)
-from aula99_package.modulo import fala_oi, soma_do_modulo
+from aula99_package.modulo import falar_oi, soma_do_modulo
 # from aula99_package.modulo import fala_oi, soma_do_modulo
 
 # print(__name__)
