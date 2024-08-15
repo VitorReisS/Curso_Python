e4ae6e1bb26f0b9eedacd6f4fd051ab002c4d23e
VitorReisS <vitor20101983@gmail.com>
2024-08-15 10:32:23 -0300

    Exercício - Adiando execução de funções

--- Programacao_Procedural_Funcional/aula101.py
+++ Programacao_Procedural_Funcional/aula101.py
@@ -2,14 +2,11 @@
 def soma(x, y):
     return x + y
 
-
 def multiplica(x, y):
     return x * y
 
-
 def criar_funcao(funcao, *args):
     return funcao(*args)
-
 
 soma_com_cinco = criar_funcao(soma, 5)
 multiplica_por_dez = criar_funcao(multiplica, 10)
