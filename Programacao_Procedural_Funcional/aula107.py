4a04b48403a9ef2e5fc9893e25ad88e0ce4de7b7
VitorReisS <vitor20101983@gmail.com>
2024-08-20 14:16:05 -0300

    Solução do Exercício + zip e zip_longest do itertools

--- Programacao_Procedural_Funcional/aula107.py
+++ Programacao_Procedural_Funcional/aula107.py
@@ -8,3 +8,17 @@
 # ['BA', 'SP', 'MG', 'RJ']
 # Resultado
 # [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+from itertools import zip_longest
+
+# def zipper(lista1, lista2):
+#     intevalo = min(len(lista1), len(lista2))
+#     return [(lista1[i], lista2[i]) for i in range(intevalo)]
+    
+
+l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+l2 = ['BA', 'SP', 'MG', 'RJ']
+
+# print(zipper(l1, l2))
+print(list(zip(l1, l2)))
+print(list(zip_longest(l1, l2, fillvalue=('SEM CIDADE'))))
