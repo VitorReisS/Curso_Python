fbffd6fbc843141c95d94489d80839390739cc2a
VitorReisS <vitor20101983@gmail.com>
2024-08-22 11:22:32 -0300

    Criando e usando um requirements.txt

--- Programacao_Procedural_Funcional/aula115.py
+++ Programacao_Procedural_Funcional/aula115.py
@@ -42,3 +42,8 @@
 
 # Congelar (ver pacotes)
 # pip freeze
+
+# Criando e usando um requirements.txt
+# pip freeze > requirements.txt
+# Instalando tudo do requirements.txt
+# pip install -r requirements.txt
