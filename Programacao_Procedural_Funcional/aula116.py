1199cd4f912a308d0847275dfd5553df7e0f84bf
VitorReisS <vitor20101983@gmail.com>
2024-08-23 08:47:39 -0300

    with open (context manager) e métodos úteis do TextIOWrapper

--- Programacao_Procedural_Funcional/aula116.py
+++ Programacao_Procedural_Funcional/aula116.py
@@ -1,4 +1,5 @@
 # Criando arquivos com Python + Context Manager with
+# with open (context manager) e métodos úteis do TextIOWrapper
 # Usamos a função open para abrir
 # um arquivo em Python (ele pode ou não existir)
 # Modos:
@@ -18,12 +19,39 @@
 # Vamos falar mais sobre o módulo json, mas:
 # json.dump = Gera um arquivo json
 # json.load
+
 caminho_arquivo = 'c:\\Users\\vt_sousa\\Desktop\\Cursos\\Curso_Python\\Programacao_Procedural_Funcional\\aula116.txt'
 
 # arquivo = open(caminho_arquivo, 'w')
 # #
 # arquivo.close()
-with open(caminho_arquivo, 'w') as arquivo:
-    print('Olá mundo')
-    print('Teste')
-    print('Arquivo vai ser fechado')
+
+# with open(caminho_arquivo, 'w') as arquivo:
+#     print('Olá mundo')
+#     print('Teste')
+#     print('Arquivo vai ser fechado')
+
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+    arquivo.seek(0, 0)
+    print(arquivo.read())
+    print('Lendo')
+    arquivo.seek(0, 0)
+    print(arquivo.readline(), end='')
+    print(arquivo.readline().strip())
+    print(arquivo.readline().strip())
+
+    print('READLINES')
+    arquivo.seek(0, 0)
+    for linha in arquivo.readlines():
+        print(linha.strip())
+
+
+print('#' * 10)
+
+with open(caminho_arquivo, 'r') as arquivo:
+    print(arquivo.read())
