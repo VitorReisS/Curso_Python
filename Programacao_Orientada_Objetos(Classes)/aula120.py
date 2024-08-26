516c213df34f44f080c769101ba9f0d789f1c7eb
VitorReisS <vitor20101983@gmail.com>
2024-08-26 10:25:30 -0300

    Introdução ao método __init__ (inicializador de atributos)

--- Programacao_Orientada_Objetos(Classes)/aula120.py
+++ Programacao_Orientada_Objetos(Classes)/aula120.py
@@ -1,4 +1,5 @@
 # class - Classes são moldes para criar novos objetos
+# Introdução ao método __init__ (inicializador de atributos)
 # As classes geram novos objetos (instâncias) que
 # podem ter seus próprios atributos e métodos.
 # Os objetos gerados pela classe podem usar seus dados
@@ -11,15 +12,18 @@
 # print(isinstance(string, str))
 
 class Pessoa:
-    ...
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
 
-p1 = Pessoa()
-p1.nome = 'Vitor'
-p1.sobrenome = 'Reis'
 
-p2 = Pessoa()
-p2.nome = 'Maria'
-p2.sobrenome = 'Joana'
+p1 = Pessoa('Vitor', 'Reis')
+# p1.nome = 'Vitor'
+# p1.sobrenome = 'Reis'
+
+p2 = Pessoa('Maria', 'Joana')
+# p2.nome = 'Maria'
+# p2.sobrenome = 'Joana'
 
 print(p1.nome)
 print(p1.sobrenome)
