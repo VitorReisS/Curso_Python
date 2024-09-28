1919428508942582939dff8d09cdaf67526f7043
VitorReisS <vitor20101983@gmail.com>
2024-09-28 10:37:34 -0300

    Código: enum.Enum (Enumerações)

--- Programacao_Orientada_Objetos(Classes)/aula157.py
+++ Programacao_Orientada_Objetos(Classes)/aula157.py
@@ -15,10 +15,35 @@
 # chave = Classe.chave.name
 # valor = Classe.chave.value
 
-def mover(direcao):
-    print(f'Movendo para {direcao}')
-    
-mover('esquerda')
-mover('direita')
-mover('acima')
-mover('abaixo')
+# def mover(direcao):
+#     print(f'Movendo para {direcao}')
+
+# mover('esquerda')
+# mover('direita')
+# mover('acima')
+# mover('abaixo')
+
+import enum
+
+# Direcoes = enum.Enum('Direcoes', ['ESQUERDA', 'DIREITA'])
+
+class Direcoes(enum.Enum):
+    ESQUERDA = enum.auto()
+    DIREITA = enum.auto()
+    ACIMA = enum.auto()
+    ABAIXO = enum.auto()
+    DIAGONAL = enum.auto()
+
+print(Direcoes(1), '|', Direcoes['ESQUERDA'],'|', Direcoes.ESQUERDA)
+print(Direcoes(1).name,'|', Direcoes.ESQUERDA.value)
+
+def mover(direcao: Direcoes):
+    if not isinstance(direcao, Direcoes):
+        raise ValueError('Direção não encontrada')
+    print(f'Movendo para {direcao.name} ({direcao.value})')
+
+mover(Direcoes.ESQUERDA)
+mover(Direcoes.DIREITA)
+mover(Direcoes.ACIMA)
+mover(Direcoes.ABAIXO)
+mover(Direcoes.DIAGONAL)
