e0ee66ffcc0a7886027ce662b6d74f1be8133b0c
VitorReisS <vitor20101983@gmail.com>
2024-10-01 11:39:59 -0300

    Solução - Criando a classe ContaPoupanca

--- Programacao_Orientada_Objetos(Classes)/aula158/contas.py
+++ Programacao_Orientada_Objetos(Classes)/aula158/contas.py
@@ -12,7 +12,27 @@ class Conta(abc.ABC):
 
     def depositar(self, valor):
         self.saldo += valor
-        self.detalhes(f'(DEPÓSITO) {valor}')
+        self.detalhes(f'(DEPÓSITO {valor})')
     
     def detalhes(self, msg=''):
-        print(f'O seu saldo é{self.saldo:.2f} {msg}')
+        print(f'O seu saldo é {self.saldo:.2f} {msg}')
+        print('------')
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
+
+        print('Não foi possível sacar o valor desejado')
+        self.detalhes(f'(SAQUE NEGADO {valor})')
+
+if __name__ == '__main__':
+    cp1 = ContaPoupanca(111, 2222, 0)
+    cp1.sacar(1)
+    cp1.depositar(1)
+    cp1.sacar(1)
+    cp1.sacar(1)
