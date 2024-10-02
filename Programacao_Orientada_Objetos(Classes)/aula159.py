b05ea8e90f1d001c44f11255736fd465bbfb99b0
VitorReisS <vitor20101983@gmail.com>
2024-10-02 10:18:38 -0300

    Valores padrão e field em dataclasses

--- Programacao_Orientada_Objetos(Classes)/aula159.py
+++ Programacao_Orientada_Objetos(Classes)/aula159.py
@@ -6,16 +6,16 @@
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
 
-from dataclasses import dataclass, asdict, astuple
+from dataclasses import dataclass, field, fields
 
 @dataclass
 class Pessoa:
-    nome: str
-    sobrenome: str
+    nome: str = field(default='MISSING', repr=False)
+    sobrenome: str = 'Not sent'
+    idade: int = 100
+    endereco: list[str] = field(default_factory=list)
 
 if __name__ == '__main__':
     p1 = Pessoa('Vitor', 'Reis')
-    print(asdict(p1).keys())
-    print(asdict(p1).values())
-    print(asdict(p1).items())
-    print(astuple(p1)[0])
+    # print(fields(p1))
+    print(p1)
