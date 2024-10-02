a224da7977c4539643781762a7da574236e395c3
VitorReisS <vitor20101983@gmail.com>
2024-10-02 09:06:26 -0300

    dataclasses com métodos, property e setter

--- Programacao_Orientada_Objetos(Classes)/aula159.py
+++ Programacao_Orientada_Objetos(Classes)/aula159.py
@@ -11,9 +11,24 @@ from dataclasses import dataclass
 @dataclass
 class Pessoa:
     nome: str
-    idade: int
+    # idade: int
+    sobrenome: str
+
+    @property
+    def nome_completo(self):
+        return f'{self.nome} {self.sobrenome}'
     
+    @nome_completo.setter
+    def nome_completo(self, valor):
+        nome, *sobrenome = valor.split()
+        self.nome = nome
+        self.sobrenome = ' '.join(sobrenome)
+
 if __name__ == '__main__':
-    p1 = Pessoa('Vitor', 26)
-    p2 = Pessoa('Vitor', 26)
-    print(p1 == p2)
+    # p1 = Pessoa('Vitor', 30)
+    # p2 = Pessoa('Vitor', 30)
+    # print(p1 == p2)
+    p1 = Pessoa('Vitor', 'Reis')
+    p1.nome_completo = 'Maria Clara'
+    print(p1)
+    print(p1.nome_completo)
