af33c7f3469c20bf92b427d93f0b250396697841
VitorReisS <vitor20101983@gmail.com>
2024-10-04 16:53:21 -0300

    Data e hora atual (now), com Unix Timestamp e Timezone diferente (pytz)

--- Modulos_Python/aula162.py
+++ Modulos_Python/aula162.py
@@ -13,11 +13,15 @@
 # pip install pytz types-pytz
 
 from datetime import datetime
+# from pytz import timezone
 
-data_str_data = '04/10/2024 07:49:23'
-# data_str_data = '04/10/2024'
-data_str_fmt = '%d/%m/%Y %H:%M:%S'
+data = datetime.now()
+print(data.timestamp())  # Isso está na base de dados
+print(datetime.fromtimestamp(1728071321))
 
-# data = datetime(2022, 4, 20, 7, 49, 23)
-data = datetime.strptime(data_str_data, data_str_fmt)
-print(data)
+# data_str_data = '2022/04/20 07:49:23'
+# data_str_data = '20/04/2022'
+# data_str_fmt = '%d/%m/%Y'
+
+# data = datetime(2022, 4, 20, 7, 49, 23, tzinfo=timezone('Asia/Tokyo'))
+# data = datetime.strptime(data_str_data, data_str_fmt)
