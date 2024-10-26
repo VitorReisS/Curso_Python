30cdc827d7018514ee9ebf1579a0fc2c22c0fe3b
VitorReisS <vitor20101983@gmail.com>
2024-10-25 23:06:44 -0300

    variáveis e método para adicionar widgets no vlayout

--- PySide6/aula202-calculadora/main.py
+++ PySide6/aula202-calculadora/main.py
@@ -1,15 +1,20 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
 from main_window import MainWindow
+from variables import WINDOW_ICON_PATH
 
 if __name__ == "__main__":
+    # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
 
-    label1 = QLabel("Teste")
-    label1.setStyleSheet('font-size: 50px;')
-    window.v_layout.addWidget(label1)
-    window.adjustFixedSize()
+    # Define o ícone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
 
+    # Executa tudo
+    window.adjustFixedSize()
     window.show()
     app.exec()
