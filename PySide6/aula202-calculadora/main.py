6bff47a22ad506b257c8af88d2b1db2785582be0
VitorReisS <vitor20101983@gmail.com>
2024-10-31 08:27:40 -0300

    Calculadora: configurando o PyQt Dark Theme (qdarktheme) no PySide6

--- PySide6/aula202-calculadora/main.py
+++ PySide6/aula202-calculadora/main.py
@@ -4,11 +4,13 @@ from display import Display
 from info import Info
 from PySide6.QtWidgets import QApplication
 from main_window import MainWindow
+from styles import setupTheme
 from variables import WINDOW_ICON_PATH
 
 if __name__ == "__main__":
     # Cria a aplicação
     app = QApplication(sys.argv)
+    setupTheme()
     window = MainWindow()
 
     # Define o ícone
