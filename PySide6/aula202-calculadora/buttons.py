898ad55f83f88f049929c4b3e21171428e0e7f0b
VitorReisS <vitor20101983@gmail.com>
2024-11-01 11:17:58 -0300

    Calculadora: permitindo apenas números válidos no display

--- PySide6/aula202-calculadora/buttons.py
+++ PySide6/aula202-calculadora/buttons.py
@@ -1,7 +1,7 @@
 from display import Display
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QGridLayout, QPushButton
-from utils import isEmpty, isNumOrDot
+from utils import isEmpty, isNumOrDot, isValidNumber
 from variables import MEDIUM_FONT_SIZE
 
 class Button(QPushButton):
@@ -14,7 +14,6 @@ class Button(QPushButton):
         font.setPixelSize(MEDIUM_FONT_SIZE)
         self.setFont(font)
         self.setMinimumSize(75, 75)
-        self.setCheckable(True)
 
 class ButtonsGrid(QGridLayout):
     def __init__(self, display: Display, *args, **kwargs) -> None:
@@ -52,5 +51,10 @@ class ButtonsGrid(QGridLayout):
         return realSlot
 
     def _insertButtonTextToDisplay(self, button):
-        button_text = button.text()
-        self.display.insert(button_text)
+        buttonText = button.text()
+        newDisplayValue = self.display.text() + buttonText
+
+        if not isValidNumber(newDisplayValue):
+            return
+        
+        self.display.insert(buttonText)
