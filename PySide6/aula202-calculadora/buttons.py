99a14ee23d79e43272de9f73af97bc46addf87c3
VitorReisS <vitor20101983@gmail.com>
2024-11-01 14:14:49 -0300

    Calculadora: Info (QLabel), TYPE_CHECKING, getter e setter e Circular Imports

--- PySide6/aula202-calculadora/buttons.py
+++ PySide6/aula202-calculadora/buttons.py
@@ -1,4 +1,4 @@
-from display import Display
+from typing import TYPE_CHECKING
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QGridLayout, QPushButton
 from utils import isEmpty, isNumOrDot, isValidNumber
@@ -6,7 +6,7 @@ from variables import MEDIUM_FONT_SIZE
 
 if TYPE_CHECKING:
     from display import Display
-    from info import
+    from info import Info
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
@@ -20,7 +20,7 @@ class Button(QPushButton):
         self.setMinimumSize(75, 75)
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, display: Display, *args, **kwargs) -> None:
+    def __init__(self, display: 'Display', info: 'Info', *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
@@ -31,7 +31,18 @@ class ButtonsGrid(QGridLayout):
             ['',  '0', '.', '='],
         ]
         self.display = display
+        self.info = info
+        self._equation = ''
         self._makeGrid()
+
+        @property
+        def equation(self):
+            return self._equation
+        
+        @equation.setter
+        def equation(self, value):
+            self._equation = value
+            self.info.setText(value)
 
     def _makeGrid(self):
         for rowNumber, rowData in enumerate(self._gridMask):
